b976cd7728f43ce8575bdd5e0af5cf43f7d1653d
maaya <maaya@DESKTOP-I90351U>
2019-09-06 14:49:10 +0300

    first commit

---
+++ CouponSystemSpring/src/main/java/com/mbms/model/Company.java
@@ -0,0 +1,23 @@
+package com.mbms.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	
+
+}
