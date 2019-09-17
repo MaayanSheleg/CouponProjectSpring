3164553d11dcaf1e66b711b437e7f586b0617a64
maaya <maaya@DESKTOP-I90351U>
2019-09-17 19:25:02 +0300

    excep.

---
+++ CouponSystemSpring/src/main/java/com/mbms/exceptions/CouponSystemException.java
@@ -0,0 +1,10 @@
+package com.mbms.exceptions;
+
+@SuppressWarnings("serial")
+public class CouponSystemException extends Exception {
+	
+	public CouponSystemException(String message) {
+		super(message);
+	}
+
+}
